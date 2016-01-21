02dd2d7a59f0c8428608248cc430312a58bc1a79
Georgi Danov <georgi.danov@gmail.com>
2016-01-21 17:23:53 +0100

    no message

--- src/chrome.clj
+++ src/chrome.clj
@@ -6,9 +6,29 @@
             
             [clj-webdriver.core :refer [start]]
             [clj-webdriver.taxi :refer :all]
+
+            [ring.adapter.jetty :as ring-j]
+            [ring.middleware.file :as ring-f]
+            [ring.middleware.resource :refer :all]
+            [ring.middleware.content-type :refer :all]
+            [ring.middleware.not-modified :refer :all]
    )  )
 
 (println "reloaded")
+
+(def handler
+  (->
+    (fn [request]
+      ; (println "request:" request)
+      {:status 200})
+    (ring-f/wrap-file ".")
+    (wrap-content-type)
+    (wrap-not-modified)))
+
+(future
+  (ring-j/run-jetty handler {:port 9000}))
+
+(println "######################\n static content server started \n ######################")
 
 (defn inject []
   (execute-script (slurp "src/inject_browser.js")))
@@ -22,7 +42,7 @@
   (inject)
 
   (execute-script "tt.fun.go()")
-
+ 
   ; (println "BODY" (time(find-element {:tag :body}))) ;; -> does not fetch the children
    ; (println "XX" (time (find-elements {:xpath "//body//*"}))) ;; -> does not give hierarchy
   )
