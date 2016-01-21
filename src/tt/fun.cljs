1d158747708ec82a93c1a88784e7cd286eda66b8
Georgi Danov <georgi.danov@gmail.com>
2016-01-21 17:20:15 +0100

    no message

--- src/tt/fun.cljs
+++ src/tt/fun.cljs
@@ -5,33 +5,59 @@
 
 (println "fun reloaded")
 
-(defn p [r input]
-  ((r (m/->CallContext nil)) input))
-
 (defn nodelist-seq
   ([nl] (nodelist-seq nl 0))
   ([nl index]
-   (reify cljs.core/ISeqable
-     (-seq [_] 
-       (reify cljs.core/ISeq
-         (-first [this]
-           (when (and nl (< index (.-length nl))) (.item nl index)))
-         (-rest [this]
-           (if (and nl (< (inc index) (.-length nl)))
-             (nodelist-seq nl (inc index))
-             '())))))))
+   (seq
+     (into []
+       (reify cljs.core/ISeqable
+         (-seq [_] 
+           (reify cljs.core/ISeq
+             (-first [this]
+               (when (and nl (< index (.-length nl))) (.item nl index)))
+             (-rest [this]
+               (if (and nl (< (inc index) (.-length nl)))
+                 (nodelist-seq nl (inc index))
+                 '())))))))))
+
+(extend-protocol IPrintWithWriter
+          js/Attr
+          (-pr-writer [attr writer opts]
+            (-write writer (str 
+                             (.-name attr) "="
+                             (.-value attr)))
+            ))
+
+(extend-protocol IPrintWithWriter
+          js/HTMLElement
+          (-pr-writer [elm writer opts]
+            (-write writer (str "["
+                             (.-tagName elm)
+                             (nodelist-seq (.-attributes elm)) "]"))))
+
+(defn p [r input]
+  ((r (m/->CallContext nil)) input))
+
+
+
+(defn it-fn
+  ^{:doc "chains the entries evaluation"}
+  [[res entries inp]]
+                                        ; (println "\nit-fn [" res entries inp "]")
+  (when (seq entries)
+    (let [r ((first entries) inp)]
+      [r (rest entries) (:stream r)])))
+
+(defn chain-ops
+  ^{:doc "chains operations so that each is invoked on the stream of the previous"}
+  [entries inp]
+  (->>
+    (iterate it-fn (it-fn [nil entries inp]))
+    (take-while some?)
+    (map first)))
 
 (rule map> [^:* entries inp]
-  (let [it-fn (fn [[res entries inp]]
-                ; (println "\nit-fn [" res entries inp "]")
-                (when (seq entries)
-                  (let [r ((first entries) inp)]
-                   [r (rest entries) (:stream r)])))
-        
-        res-strm (->>
-                   (iterate it-fn (it-fn [nil entries inp]))
-                   (take-while some?)
-                   (map first))
+  (let [res-strm (chain-ops entries inp)
         ; _ (println "\nXX:[" res-strm "|" entries inp ((first entries) inp) "]")
         res-processed (reduce
                         (fn [o res]
@@ -60,7 +86,7 @@
   (assert (or (nil? op) (fn? op)))
   (assert (some? elem))
   
-  (js/console.log "node:" selector elem)
+  ; (js/console.log "node:" selector elem)
 
   (let [node (.querySelector elem selector)]
     (if node
@@ -69,6 +95,16 @@
         (m/match node node))
       (m/nomatch nil))))
 
+(rule >nodes [selector ^:* op elem]
+  (assert (string? selector) selector)
+  (assert (fn? op) op)
+  (assert (some? elem))
+  
+  (let [nodes (nodelist-seq (.querySelectorAll elem selector))]
+      (if (seq nodes)
+      (op nodes)
+      (m/nomatch elem))))
+
 (rule >text [element]
   (assert element)
   (m/match (.innerHTML element) element))
@@ -76,9 +112,15 @@
 (rule end [a inp]
   (m/match :end inp))
 
-(rule fun> [fun ^:* r inp]
+(rule intercept>
+  [fun ^:* op inp]
+  (fun (op inp) inp))
+
+(rule fun>
+  ^{:doc "applies fun on the value of the result of the wrapped operation. does not care if it's match or not"}
+  [fun ^:* r inp]
   (let [res (r inp)]
-    (m/->Result (fun (:val res) (:stream res) (:match res)))))
+    (m/->Result (fun (:val res)) (:stream res) (:match res))))
 
 (rule >fun [match-fn fun inp]
   (let [r (fun inp)]
@@ -97,6 +139,23 @@
 
 ; (((node "p" (end nil)) (m/->CallContext nil))
  ; js/document.documentElement)
+
+(rule >eq [target ^:* fun input]
+  (let [res (fun input)]
+    (if (and (m/matches? res) (= target (:val res)))
+      (m/match target (:stream res))
+      (m/nomatch input))))
+
+(rule  >some
+  ^{:doc "applies op to the input sequence. matches when at least one element matches op. returns seq of the matches"}
+  [^:* op input]
+  (assert (seq? input) input)
+  (assert (fn? op))
+  
+  (let [res (filter m/matches? (map op input))]
+    (if (seq res)
+      (m/match res (:stream (last res)))
+      (m/nomatch input))))
 
 ;; TODO see Node.nodeType
 (defn text-node? [n] (= (.-prototype js/Text) (js/Object.getPrototypeOf n)))
@@ -140,6 +199,29 @@
 ; (run-tests)
 (run-tests (cljs.test/empty-env :cljs.test/pprint))
 
+(p
+  (>nodes "h3"
+    (>some
+      (>node "span"
+        (>eq ":inbound" inner-html))))
+  js/document.documentElement)
+
+;;TODO intercept-result and intercept-inp
+(defn parent-node  [res inp]
+                                        ; (println "\n!!!" inp res)
+  (if (m/matches? res)
+    (m/match (.-parentNode inp) nil)
+    res))
+
+(cljs.pprint/pprint
+  (p
+    (>nodes "div h3"
+      (>some
+        ;;TODO cheap parent-node> without rule?
+        (intercept> parent-node
+          (>node "span"
+            (>eq "outbound:" inner-html)))))
+    js/document.documentElement))
 
 ; (->> (p (>node "h3.fl_0"
                   ; (>child-node
