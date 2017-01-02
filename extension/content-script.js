b798d18d255dadd97f6c4c4db4fbb730ed60bf34
Georgi Danov <georgi.danov@gmail.com>
2017-01-02 23:22:40 +0100

    prevent base.js from interfering with the page

--- extension/content-script.js
+++ extension/content-script.js
@@ -1,4 +1,5 @@
-
+// mandatory otherwise base.js injects the deps.js into the page and messes it up
+CLOSURE_IMPORT_SCRIPT = (src) => {console.log ("content script: CLOSURE_IMPORT_SCRIPT will not load",src)}
 
 
 
